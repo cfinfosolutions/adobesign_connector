4b1fb7eb731d3a68f072febf2e0b564c59b189d1
ananthu krishna <ananthu.krishna@gmail.com>
2023-01-06 13:00:05 +0400

    updates

--- adobesign_connector/models/account_move.py
+++ adobesign_connector/models/account_move.py
@@ -19,7 +19,7 @@ class AccountMove(models.Model):
     def get_report_file(self):
         file = None
         for rec in self:
-            report = self.env.ref('account.account_invoices')._render_qweb_pdf([rec.id])
+            report = self.env.ref('account.account_invoices').render_qweb_pdf([rec.id])
             if report:
                 datas =base64.b64encode(report[0])
                 attachment = self.env['ir.attachment'].create({
